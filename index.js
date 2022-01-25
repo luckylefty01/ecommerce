6038cc12e623a1a619b735e9ba23e3250a8b291b
James Torres <james.torres01@gmail.com>
2022-01-25 13:25:08 -0500

    added env, server connected '

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 // dependencies
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
+dotenv.config();
+// server connection
+mongoose.connect(process.env.MONGO_URL)
+.then(() => console.log("DB connected!"))
+.catch((err) => {
+    console.log(error);
+})
 
 // middleware
 
