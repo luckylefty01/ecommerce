7f76bd1a46924ec3691b6b1e38b1818b872f8899
James Torres <james.torres01@gmail.com>
2022-01-25 13:05:31 -0500

    setting up server

---
+++ index.js
@@ -0,0 +1,11 @@
+// dependencies
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose")
+
+
+// middleware
+
+app.listen(3000, () => {
+    console.log("server is live");
+});
