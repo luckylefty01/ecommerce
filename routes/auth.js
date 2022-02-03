e1f067256b6843cff0c6c87bea6cc9614584e3ab
James Torres <james.torres01@gmail.com>
2022-02-03 11:30:09 -0500

    JWT working, tested in postman

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
-const User = require("../models/User")
-const CryptoJS = require("crypto-js")
+const User = require("../models/User");
+const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 // register route
 router.post("/register", async (req , res) => {
@@ -32,9 +33,19 @@ router.post("/login", async (req, res) => {
 
         originalPassword !== req.body.password && res.status(401).json("Incorrect Password!");
 
+        // jwt token
+        const accessToken = jwt.sign({
+            id: user._id,
+            isAdmin: user.isAdmin,
+        
+        }, 
+        process.env.JWT_KEY,
+        {expiresIn: "1d"} // this will have the key expire in 1 day and the user will have to log back in.
+        );
+
         const {password, ...others} = user._doc;
 
-        res.status(200).json(others);
+        res.status(200).json({...others, accessToken});
     } catch (err) {
         res.status(500).json(err);
     }
