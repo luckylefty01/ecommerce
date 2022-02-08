c86393dd0cd823a3fdc2242b5d19f7702eaecaac
James Torres <james.torres01@gmail.com>
2022-02-07 23:24:16 -0500

    fixed auth route, can make new users and log in

--- routes/auth.js
+++ routes/auth.js
@@ -3,53 +3,64 @@ const User = require("../models/User");
 const CryptoJS = require("crypto-js");
 const jwt = require("jsonwebtoken");
 
-// register route
-router.post("/register", async (req , res) => {
-    const newUser = new User(
-        {
+//register/create new user route
+router.post("/register", async (req, res) => {
+  const newUser = new User(
+      {
         username: req.body.username,
         email: req.body.email,
-        password: CryptoJS.AES.decrypt("req.body.password", "process.env.PASS_KEY").toString(),
+        password: CryptoJS.AES.encrypt(req.body.password,process.env.PASS_KEY).toString(),
     });
 
-    try{
-        const savedUser = await newUser.save()
-        res.status(201).json(savedUser);    
-    } catch (err) {
-        res.status(500).json(err);
-    }
+  try {
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-// login route
-// used CryptoJS to encrypt passwords for users
-router.post("/login", async (req, res) => {
-    try {
-        const user = await User.findOne({ username: req.body.username });
+//login route
+//used CryptoJS to encrypt passwords for users
+router.post('/login', async (req, res) => {
+    try{
+        const user = await User.findOne(
+            {
+                userName: req.body.user_name
+            }
+        );
 
         !user && res.status(401).json("Incorrect User Name!");
 
-        const hashPassword = CryptoJS.AES.decrypt(user.password, process.env.PASS_KEY);
-        const originalPassword = hashPassword.toString(CryptoJS.enc.Utf8);
-
-        originalPassword !== req.body.password && res.status(401).json("Incorrect Password!");
-
-        // jwt token
-        const accessToken = jwt.sign({
-            id: user._id,
-            isAdmin: user.isAdmin,
-        
-        }, 
-        process.env.JWT_KEY,
-        {expiresIn: "1d"} // this will have the key expire in 1 day and the user will have to log back in.
+        const hashedPassword = CryptoJS.AES.decrypt(
+            user.password,
+            process.env.PASS_KEY
         );
 
-        const {password, ...others} = user._doc;
 
+        const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+        const inputPassword = req.body.password;
+        
+        originalPassword != inputPassword && 
+            res.status(401).json("Incorrect Password!");
+
+        const accessToken = jwt.sign(
+        {
+            id: user._id,
+            isAdmin: user.isAdmin,
+        },
+        process.env.JWT_KEY,
+            {expiresIn:"3d"}
+        );
+  
+        const { password, ...others } = user._doc;  
         res.status(200).json({...others, accessToken});
-    } catch (err) {
+
+    }catch(err){
         res.status(500).json(err);
     }
+
 });
 
-
-module.exports = router
+module.exports = router;
